30409971be062159bf237efb83f84877cd70d3ae
unknown <khalidhasan842@gmail.com>
2021-01-04 23:12:25 +0600

    final term

---
+++ Book management studio/Book management studio/Books.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_management_studio
+{
+    class Books
+    {
+
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string Edition { get; set; }
+    }
+}
---
+++ Book management studio/Book management studio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Book_management_studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
